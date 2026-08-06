b51b65d7b724f7aaee897c4b3be441ea1a039c52
diegos231091-cpu <diegos231091@gmail.com>
2026-08-06 14:41:08 +0200

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+BASE_URL = "https://icp.administracionelectronica.gob.es/icpplus/index.html"
+
+PROVINCIA = "Barcelona"
+TRAMITE = "POLICÍA - TOMA DE HUELLAS"
+
+CHECK_INTERVAL = 300
